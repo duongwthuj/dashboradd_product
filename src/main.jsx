a72f5ee46ffdfe6e8661d2b6d800687787a4060c
duongwthuj <thunguyenduong37@gmail.com>
2025-12-27 23:35:11 +0700

    chore: add version log for debugging

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+console.log('VERSION: 2025-12-27 V3 - Cached Buster Active');
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
